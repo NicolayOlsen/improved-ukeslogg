32529d88d3968b01540811e15b3ba44cc85f8320
NicolayOlsen <nicolay2003.nao@gmail.com>
2023-02-05 03:40:21 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,35 +2,86 @@ import React, { useState } from 'react';
 import "./App.css"
 
 function App() {
+    const [weekNumber, setWeekNumber] = useState('');
+    const [fridayText, setFridayText] = useState('');
+    const [mondayText, setMondayText] = useState('');
+    const [tuesdayText, setTuesdayText] = useState('');
+    const [wednesdayText, setWednesdayText] = useState('');
+    const [thursdayText, setThursdayText] = useState('');
     const [data, setData] = useState(null);
-    const [inputText, setInputText] = useState('');
   
     const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      const response = await fetch('http://127.0.0.1:5000/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({inputText: inputText})
-    });
-  
-      const json = await response.json();
-      setData(json);
-    };
-  
-    return (
-    <div className='card'>
-      <div className='inputarea'>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={inputText} onChange={e => setInputText(e.target.value)} />
-          <button type="submit">Generate Document</button>
-        </form>
-        {data && <p>Generated file: {data.fileName}</p>}
+        event.preventDefault();
+      
+          // Call the API with all the text inputs as a single object
+          const inputText = {
+              weekNumber,
+              fridayText,
+              mondayText,
+              tuesdayText,
+              wednesdayText,
+              thursdayText,
+          };
+      
+        const response = await fetch('http://127.0.0.1:5000/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(inputText)
+      });
+      
+        const json = await response.json();
+        setData(json);
+      };
+      
+      return (
+      <div className='calender'>
+        <div className='friday'>
+          <h1>Uke</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={weekNumber} onChange={e => setWeekNumber(e.target.value)} />
+            <br/>
+          </form>
         </div>
-      </div>
-    );
-  }
-  
-  export default App;
+        <div className='friday'>
+          <h1>Fredag</h1>
+          <form onSubmit={handleSubmit}>
+            <textarea type="text" value={fridayText} onChange={e => setFridayText(e.target.value)} />
+            <br/>
+          </form>
+        </div>
+        <div className='friday'>
+          <h1>Mandag</h1>
+          <form onSubmit={handleSubmit}>
+            <textarea type="text" value={mondayText} onChange={e => setMondayText(e.target.value)} />
+            <br/>
+          </form>
+        </div>
+        <div className='friday'>
+          <h1>Tirsdag</h1>
+          <form onSubmit={handleSubmit}>
+            <textarea type="text" value={tuesdayText} onChange={e => setTuesdayText(e.target.value)} />
+            <br/>
+          </form>
+        </div>
+        <div className='friday'>
+          <h1>Onsdag</h1>
+          <form onSubmit={handleSubmit}>
+            <textarea type="text" value={wednesdayText} onChange={e => setWednesdayText(e.target.value)} />
+            <br/>
+          </form>
+        </div>
+        <div className='friday'>
+          <h1>Torsdag</h1>
+          <form onSubmit={handleSubmit}>
+            <textarea type="text" value={thursdayText} onChange={e => setThursdayText(e.target.value)} />
+            <br/>
+            <button type="submit">GENERER UKESLOGG</button>
+          </form>
+        </div>
+        {data && <p>Generert fil: {data.fileName}</p>}
+    </div>
+);
+}
+export default App;
--- src/app.py
+++ src/app.py
@@ -6,17 +6,31 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        input_text = request.get_json()['inputText']
+        inputs = request.get_json()
+        week_number = inputs.get('weekNumber', '')
+        friday_text = inputs.get('fridayText', '')
+        monday_text = inputs.get('mondayText', '')
+        tuesday_text = inputs.get('tuesdayText', '')
+        wednesday_text = inputs.get('wednesdayText', '')
+        thursday_text = inputs.get('thursdayText', '')
 
         # Load the template
         doc = docxtpl.DocxTemplate("ukeslogg.docx")
 
         # Insert the input text into the template
-        context = {'inputText': input_text}
+        context = {
+          'weekNumber': week_number,
+          'fridayText': friday_text,
+          'mondayText': monday_text,
+          'tuesdayText': tuesday_text,
+          'wednesdayText': wednesday_text,
+          'thursdayText': thursday_text
+        }
+
         doc.render(context)
 
         # Save the new Word document
-        file_name = "generated-document.docx"
+        file_name = "Ukeslogg uke {}.docx".format(week_number)
         doc.save(file_name)
 
         # Return the file name to the frontend
@@ -29,7 +43,6 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
-
 
 if __name__ == '__main__':
     app.run(debug=True)
